9ae9f439048d5c2826e627be991af27658afba4a
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 07:03:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,9 @@
 def prime?(num)
   range = 2..num-1
   for x in range
-  puts "#{num} % #{x}"
     if num % x == 0 
-      puts false
       return false 
     end
   end
-  puts true
   return true
 end
-
-prime?(5)
-prime?(10)
